6fd1dc42ebeb8471b403cb47d924510a80e69e99
AnleList <96717447+AnleList@users.noreply.github.com>
2026-09-14 00:39:15 +0300

    Revert "homework_1"

--- homework_1/home_work_1.4.py
+++
@@ -1,4 +0,0 @@
-full_name = 'Ivanou Ivan'
-
-last_name, first_name = full_name.split()
-print(f"{first_name} {last_name}")
--- homework_1/home_work_1.5.py
+++
@@ -1,3 +0,0 @@
-trimmed_string = ' строка с пробелами в начале и в конце '
-
-print(trimmed_string.strip())

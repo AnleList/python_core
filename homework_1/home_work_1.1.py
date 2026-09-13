ad6d47583c647a6ee321ab4ec580a7c14bc39fd3
anle <anle@list.ru>
2026-09-13 23:03:46 +0300

    make home_work_1 done

---
+++ homework_1/home_work_1.1.py
@@ -0,0 +1,5 @@
+number_for_round_1 = -1.6
+number_for_round_2 = 2.99
+
+print(round(number_for_round_1))
+print(round(number_for_round_2))
---
+++ homework_1/home_work_1.2.py
@@ -0,0 +1,2 @@
+mutable_string = 'www.my_site.com#about'
+print(mutable_string)

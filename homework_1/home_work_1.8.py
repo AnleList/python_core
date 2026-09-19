259f8d0b492243cac01e7c29b7a3748ced4c2346
anle <anle@list.ru>
2026-09-19 21:49:59 +0300

    resolve home_work_1.8

---
+++ homework_1/home_work_1.8.py
@@ -0,0 +1,10 @@
+string_1 = "employ"
+string_2 = "employment"
+
+# Выполняем проверку
+if string_1 in string_2:
+    # если совпало выводим что входит:
+    print(f"строка: {string_1} входит в строку {string_2}")
+else:
+    # если не совпало выводим что не входит:
+    print(f"строка: {string_1} не входит в строку {string_2}")
